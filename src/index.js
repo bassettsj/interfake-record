3fafc6df7de87f3a21031c7b5ded7d4fa2b82617
Steven Bassett <steven.j.bassett@gmail.com>
2015-05-25 16:15:22 -0700

    change how to record fixtures

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 'use strict';
 
-import Interfake from 'interfake';
+import http from 'http';
+import httpProxy from 'http-proxy';
+import fs from 'fs';
+import path from 'path';
 
+export default class FixtureRecorder {
+  constructor (target, directory = process.cwd()) {
 
-export default class InterfakeRecorder extends Interfake {
-  constructor(options) {
-    super(options);
+    this.directory = this.dir = path.normalize(directory);
+    fs.exists(this.dir, (exists) => {
+      if (!exists) {
+        throw new Error('directory does not exists: ' + this.dir);
+      }
+    })
+
+    let proxy = this.proxy = httpProxy.createProxyServer({
+      target: target
+    });
+
+    proxy.on('proxyReq', (proxyReq, req, res, options) => {
+      console.log(this.dir, req.url);
+      let recordResStream = fs.createWriteStream(path.join(this.dir, req.url));
+      res.pipe(recordResStream);
+    });
+
+  }
+
+  listen (port) {
+    console.log(`Listening on port ${port}`);
+    this.proxy.listen(...arguments);
   }
 }
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(4515);
+
+
+var fixture = new FixtureRecorder('http://127.0.0.1:4515', __dirname + '/../.temp');
+
+fixture.listen(3000);
--- test/index_test.js
+++ test/index_test.js
@@ -1,20 +1,46 @@
+/* global describe, beforeEach, afterEach, it */
 'use strict';
 
 import {expect} from 'chai';
+import request from 'request';
+import Q from 'q';
+import Interfake from 'interfake';
 import InterfakeRecord from '../lib/index';
 
+
 describe('InterfakeRecord', () => {
-  var interfake;
-  beforeEach(() => interfake = new InterfakeRecord());
+  var interfake, remote;
+  beforeEach(() => {
+    remote = new Interfake();
+    remote.get('/').status(200).body({whatever: 'stuff'});
+    remote.listen(2000);
+    interfake = new InterfakeRecord();
+  });
   afterEach(() => {
     if (interfake) {
       interfake.stop();
     }
+    if (remote) {
+      remote.stop();
+    }
   });
-  it('should extend the base class', () => {
-    expect(interfake).to.be.defined;
+  it('should have intercept method', () => {
+    debugger;
+    expect(interfake).to.have.property('intercept');
+  });
+  it('should proxy a remote url', (done) => {
+    interfake.intercept({
+      route: '/',
+      remote: 'http://127.0.0.1:2000',
+      folder: __dirname + '/.temp'
+    });
+    interfake.listen(3000);
 
-
+    request('http:127.0.0.1:3000', function (err, res, body) {
+      expect(body).to.have.property('whatever', 'stuff');
+      done();
+    });
   });
 
-})
+
+});
